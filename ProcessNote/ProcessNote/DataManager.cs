75772d60399282e05465b1cb236ccd607d8d8347
Erik Szobonya <szobonyaerik@gmail.com>
2020-01-23 09:53:26 +0100

    Done

--- ProcessNote/ProcessNote/DataManager.cs
+++ ProcessNote/ProcessNote/DataManager.cs
@@ -9,54 +9,6 @@ namespace ProcessNote
 {
     class DataManager
     {
-        /*
-        public void WriteXML(string filename, List<Target> tasks)
-        {
-            string text = "";
-            if (tasks.Count >= 1)
-            {
-                text += "<Tasks>\n";
-                foreach (Target task in tasks)
-                {
-                    text += $"\t<{task.name}>\n";
-                    text += $"\t\t<ID>{task.id}</ID>\n" +
-                            $"\t\t<CPU>{task.cpu}</CPU>\n" +
-                            $"\t\t<RAM>{task.ram}</RAM>\n" +
-                            $"\t\t<Runtime>{task.runtime}</Runtime>\n" +
-                            $"\t\t<StartTime>{task.start}</StartTime>\n" +
-                            $"\t\t<Comment>{task.comment}</Comment>\n";
-                    text += $"\t</{task.name}>\n";
-                }
-                text += "</Tasks>";
-                File.WriteAllText(filename, text);
-            }
-        }
-        public List<string> ReadXML(string filename)
-        {
-            XmlTextReader reader = new XmlTextReader(filename);
-            List<string> properties = new List<string>();
-
-            while (reader.Read())
-            {
-                switch (reader.NodeType)
-                {
-                    case XmlNodeType.Element:
-                        break;
-
-
-                    case XmlNodeType.Text:
-                        properties.Add(reader.Value);
-
-
-                        break;
-                    case XmlNodeType.EndElement:
-                        break;
-                }
-            }
-            return properties;
-        }
-        */
-
         public void XmlWriter(string filename, Target[] tasks)
         {
             XmlSerializer writer = new XmlSerializer(typeof(Target[]));
@@ -66,7 +18,6 @@ namespace ProcessNote
                 writer.Serialize(writerfinal, tasks);
             }
         }
-
         public List<Target> XmlReader(string filename)
         {
             XmlSerializer reader = new XmlSerializer(typeof(List<Target>));
